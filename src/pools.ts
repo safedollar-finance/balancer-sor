65a4e59f6c10036141cbf85d04aba9aecefbe561
johngrantuk <johngrantuk@googlemail.com>
2021-08-04 11:19:54 +0100

    Handle unknown pool types gracefully.

--- src/pools.ts
+++ src/pools.ts
@@ -92,8 +92,12 @@ export function filterPoolsOfInterest(
                 pool.baseToken
             );
             newPool.setCurrentBlockTimestamp(currentBlockTimestamp);
-        } else
-            throw `Unknown pool type or type field missing: ${pool.poolType}`;
+        } else {
+            console.error(
+                `Unknown pool type or type field missing: ${pool.poolType} ${pool.id}`
+            );
+            return;
+        }
 
         let tokenListSet = new Set(pool.tokensList);
         // Depending on env file, we add the BPT as well as
